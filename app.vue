6cdf410fcb2f978a3c3fe99127e5f56108c9c2de
MarkoPvlkvc <pavlakovic.marko@gmail.com>
2024-05-15 13:33:45 +0200

    initial-files

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-between px-10">
+    <p class="m-0 font-bold">{Finsweet</p>
+      <div>
+        lsladlsdlaldal
+      </div>
+  </div>
+  <NuxtPage />
+</template>
